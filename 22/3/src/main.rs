8fa831fd0ca5c3957aac07fb41ab243fdf3d0391
MrSpoony <kimi.loeffel@hispeed.ch>
2022-12-03 20:02:28 +0100

    formatting

--- 22/3/src/main.rs
+++ 22/3/src/main.rs
@@ -4,37 +4,37 @@ fn main() {
     println!(
         "{}",
         include_str!("../input.txt")
-        .lines()
-        .map(|line| {
-            let mut set = HashSet::new();
-            for (i, char) in line.chars().into_iter().enumerate() {
-                if i < line.len()/2 {
-                    set.insert(char);
-                } else if set.contains(&char) {
-                    return char_to_int(char)
+            .lines()
+            .map(|line| {
+                let mut set = HashSet::new();
+                for (i, char) in line.chars().into_iter().enumerate() {
+                    if i < line.len() / 2 {
+                        set.insert(char);
+                    } else if set.contains(&char) {
+                        return char_to_int(char);
+                    }
                 }
-            }
-            assert!(false, "Unreachable");
-            return char_to_int('A')
-        })
-        .sum::<u32>()
+                assert!(false, "Unreachable");
+                return char_to_int('A');
+            })
+            .sum::<u32>()
     );
     let lines = include_str!("../input.txt").lines();
     let mut sets: Vec<Vec<HashSet<char>>> = Vec::new();
     let mut sum = 0;
     for (i, line) in lines.into_iter().enumerate() {
-        let set_nr = i/3;
-        if i%3 == 0 {
+        let set_nr = i / 3;
+        if i % 3 == 0 {
             sets.push(Vec::new());
             sets[set_nr].push(HashSet::new());
             sets[set_nr].push(HashSet::new());
         }
         for char in line.chars() {
-            if i%3 == 0 {
+            if i % 3 == 0 {
                 sets[set_nr][0].insert(char);
-            } else if i%3 == 1 && sets[set_nr][0].contains(&char) {
+            } else if i % 3 == 1 && sets[set_nr][0].contains(&char) {
                 sets[set_nr][1].insert(char);
-            } else if i%3 == 2 && sets[set_nr][1].contains(&char) {
+            } else if i % 3 == 2 && sets[set_nr][1].contains(&char) {
                 sum += char_to_int(char);
                 break;
             }
