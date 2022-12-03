b258568fa154d688de194964a7b0454326e5c0b9
MrSpoony <kimi.loeffel@hispeed.ch>
2022-12-03 14:23:06 +0100

    simplify rust code

--- 22/2/src/main.rs
+++ 22/2/src/main.rs
@@ -23,30 +23,14 @@ fn main() {
         "{}",
         converted_lines
             .iter()
-            .map(|line| {
-                return (match (line[1] - line[0] + 3) % 3 {
-                    0 => 3,
-                    1 => 6,
-                    2 => 0,
-                    _ => 0,
-                }) + line[1]
-                    + 1;
-            })
+            .map(|line| (line[1] - line[0] + 4) % 3 * 3 + line[1] + 1)
             .sum::<i32>()
     );
     println!(
         "{}",
         converted_lines
             .iter()
-            .map(|line| {
-                return (match line[1] {
-                    0 => 0 + ((line[0]+2)%3),
-                    1 => 3 + line[0],
-                    2 => 6 + ((line[0]+1)%3),
-                    _ => 0,
-                })
-                    + 1;
-            })
+            .map(|line| (line[0] + line[1] + 2) % 3 + line[1] * 3 + 1)
             .sum::<i32>()
-    )
+    );
 }
