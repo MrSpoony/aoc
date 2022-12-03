d5a06a2636d10065a1da7dfb7e7863a57fe40f34
MrSpoony <kimi.loeffel@hispeed.ch>
2022-12-03 14:24:46 +0100

    simplify rust code

--- 22/1/src/main.rs
+++ 22/1/src/main.rs
@@ -1,16 +1,13 @@
 fn main() {
-    println!(
-        "{}", 
-        include_str!("../input.txt")
-        .split("\n\n")
-        .map(|game| game.lines().map(|val| val.parse::<i32>().unwrap()).sum::<i32>())
-        .max()
-        .unwrap()
-    );
     let mut nums = include_str!("../input.txt")
         .split("\n\n")
-        .map(|game| game.lines().map(|val| val.parse::<i32>().unwrap()).sum::<i32>())
+        .map(|game| {
+            game.lines()
+                .map(|val| val.parse::<i32>().unwrap())
+                .sum::<i32>()
+        })
         .collect::<Vec<i32>>();
     nums.sort_by(|x, y| y.cmp(x));
+    println!("{}", nums.clone().into_iter().max().unwrap());
     println!("{}", nums[0..3].iter().sum::<i32>());
 }
