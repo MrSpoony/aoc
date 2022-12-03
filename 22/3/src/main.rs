f75ac1505e4ec5e34a0614863a993fba0f8dcf68
MrSpoony <kimi.loeffel@hispeed.ch>
2022-12-03 20:38:58 +0100

    just learned about match

--- 22/3/src/main.rs
+++ 22/3/src/main.rs
@@ -1,10 +1,11 @@
 use std::collections::HashSet;
 
 fn main() {
+    let lines = include_str!("../input.txt").lines();
     println!(
         "{}",
-        include_str!("../input.txt")
-            .lines()
+        lines
+            .clone()
             .map(|line| {
                 let mut set = HashSet::new();
                 for (i, char) in line.chars().into_iter().enumerate() {
@@ -14,41 +15,38 @@ fn main() {
                         return char_to_int(char);
                     }
                 }
-                assert!(false, "Unreachable");
-                return char_to_int('A');
+                unreachable!();
             })
             .sum::<u32>()
     );
-    let lines = include_str!("../input.txt").lines();
-    let mut sets: Vec<Vec<HashSet<char>>> = Vec::new();
+    let mut sets: Vec<Vec<HashSet<char>>> =
+        vec![vec![HashSet::new(); 2]; lines.clone().count() / 3];
     let mut sum = 0;
     for (i, line) in lines.into_iter().enumerate() {
         let set_nr = i / 3;
-        if i % 3 == 0 {
-            sets.push(Vec::new());
-            sets[set_nr].push(HashSet::new());
-            sets[set_nr].push(HashSet::new());
-        }
         for char in line.chars() {
-            if i % 3 == 0 {
-                sets[set_nr][0].insert(char);
-            } else if i % 3 == 1 && sets[set_nr][0].contains(&char) {
-                sets[set_nr][1].insert(char);
-            } else if i % 3 == 2 && sets[set_nr][1].contains(&char) {
-                sum += char_to_int(char);
-                break;
-            }
+            match i % 3 {
+                0 => {
+                    sets[set_nr][0].insert(char);
+                }
+                1 if sets[set_nr][0].contains(&char) => {
+                    sets[set_nr][1].insert(char);
+                }
+                2 if sets[set_nr][1].contains(&char) => {
+                    sum += char_to_int(char);
+                    break;
+                }
+                _ => (),
+            };
         }
     }
     println!("{}", sum);
 }
 
 fn char_to_int(c: char) -> u32 {
-    if c >= 'a' && c <= 'z' {
-        c as u32 - 'a' as u32 + 1
-    } else if c >= 'A' && c <= 'Z' {
-        c as u32 - 'A' as u32 + 27
-    } else {
-        panic!("Invalid char");
+    match c {
+        'a'..='z' => c as u32 - 'a' as u32 + 1,
+        'A'..='Z' => c as u32 - 'A' as u32 + 27,
+        _ => unreachable!("Invalid char"),
     }
 }
