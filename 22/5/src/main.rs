5ad49a57fdf9fa7e399091ea34c5919d46036da1
MrSpoony <kimi.loeffel@hispeed.ch>
2022-12-05 20:26:41 +0100

    day 5

---
+++ 22/5/src/main.rs
@@ -0,0 +1,69 @@
+fn main() {
+    let text = include_str!("../input.txt")
+        .split("\n\n")
+        .collect::<Vec<_>>();
+    let instructions = text[1]
+        .lines()
+        .map(|line| {
+            line.split(" ")
+                .map(|x| x.parse::<i32>().ok())
+                .filter(|x| x.is_some())
+                .map(|x| x.unwrap())
+                .collect::<Vec<i32>>()
+        })
+        .collect::<Vec<Vec<i32>>>();
+    let midstacks: Vec<Vec<char>> = text[0]
+        .lines()
+        .rev()
+        .skip(1)
+        .map(|line| {
+            line.chars()
+                .enumerate()
+                .filter(|&(i, _)| i % 4 == 1)
+                .map(|(_, v)| v)
+                .collect()
+        })
+        .collect();
+    let mut stacks: Vec<Vec<char>> = vec![];
+    for midstack in midstacks {
+        for (i, c) in midstack.iter().enumerate() {
+            if i >= stacks.len() {
+                stacks.push(vec![]);
+            }
+            if *c != ' ' {
+                stacks[i].push(*c);
+            }
+        }
+    }
+    let mut st1 = stacks.clone();
+    let mut st2 = stacks.clone();
+    for instruction in instructions {
+        let cnt = instruction[0];
+        let from = (instruction[1] - 1) as usize;
+        let to = (instruction[2] - 1) as usize;
+        let mut to_move = vec![];
+        for _ in 0..cnt {
+            let what1 = st1[from][st1[from].len() - 1];
+            let what2 = st2[from][st2[from].len() - 1];
+            st1[from].pop();
+            st2[from].pop();
+            st1[to].push(what1);
+            to_move.push(what2);
+        }
+        for c in to_move.iter().rev() {
+            st2[to].push(*c);
+        }
+    }
+    println!(
+        "{}",
+        st1.iter()
+            .map(|stack| stack[stack.len() - 1])
+            .collect::<String>()
+    );
+    println!(
+        "{}",
+        st2.iter()
+            .map(|stack| stack[stack.len() - 1])
+            .collect::<String>()
+    );
+}
