09e78664e996a89efa3d5b2821baf8f1ece98070
MrSpoony <kimi.loeffel@hispeed.ch>
2022-12-14 22:39:35 +0100

    day 14 p2

--- 22/14/src/main.rs
+++ 22/14/src/main.rs
@@ -1,8 +1,9 @@
 use std::{collections::HashSet, str::FromStr};
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Cave {
     map: HashSet<(i32, i32)>,
+    floor: i32,
 }
 
 impl FromStr for Cave {
@@ -10,6 +11,7 @@ impl FromStr for Cave {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let mut cave = Cave {
             map: HashSet::new(),
+            floor: 0,
         };
         s.lines()
             .map(|line| {
@@ -29,23 +31,57 @@ impl FromStr for Cave {
                     }
                 }
             });
+        cave.floor = cave.map.iter().map(|(_, y)| y).max().unwrap() + 2;
         return Ok(cave);
     }
 }
 
 impl Cave {
-    fn simulate(&mut self, start: (i32, i32)) -> usize {
+    fn simulate_infinite(&mut self, start: (i32, i32)) -> usize {
         let mut res = 0;
-        while self.drop_one(start) {
+        while self.drop_one_inf(start) {
             res += 1;
         }
         res
     }
 
-    fn drop_one(&mut self, start: (i32, i32)) -> bool {
+    fn simulate_floor(&mut self, start: (i32, i32)) -> usize {
+        let mut res = 0;
+        while self.get(&start).is_none() {
+            self.drop_one_floor(start);
+            res += 1;
+        }
+        res
+    }
+
+    fn drop_one_floor(&mut self, start: (i32, i32)) {
+        let mut curr = (start.0, start.1 - 1);
+        'outer: loop {
+            if curr.1 + 1 >= self.floor {
+                break;
+            }
+            for i in vec![0, -1, 1].iter() {
+                if self.get(&(curr.0 + i, curr.1 + 1)).is_none() {
+                    curr = (curr.0 + i, curr.1 + 1);
+                    continue 'outer;
+                }
+            }
+            break;
+        }
+        self.map.insert(curr);
+    }
+
+    fn get(&self, pos: &(i32, i32)) -> Option<(i32, i32)> {
+        if pos.1 >= self.floor {
+            return Some(*pos);
+        }
+        return self.map.get(&pos).copied();
+    }
+
+    fn drop_one_inf(&mut self, start: (i32, i32)) -> bool {
         let mut curr = start;
         'outer: loop {
-            if curr.1 >= 1000 {
+            if curr.1 >= self.floor {
                 return false;
             }
             for i in vec![0, -1, 1].iter() {
@@ -59,9 +95,22 @@ impl Cave {
         }
         true
     }
+
+    fn _print(self) {
+        for i in 480..=520 {
+            for j in 0..=25 {
+                match self.map.get(&(i, j)) {
+                    Some(_) => print!("#"),
+                    None => print!("."),
+                }
+            }
+            println!();
+        }
+    }
 }
 
 fn main() {
     let mut cave = Cave::from_str(include_str!("../input.txt")).unwrap();
-    println!("{}", cave.simulate((500, 0)));
+    println!("{}", cave.clone().simulate_infinite((500, 0)));
+    println!("{}", cave.simulate_floor((500, 0)));
 }
